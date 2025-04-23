0c28bfff049ca28a38ec68925f898d8d1ddbe19c
Leticia Eto Filó Candido <160032869+Leticia-Eto@users.noreply.github.com>
2025-04-23 08:34:06 -0300

    Add files via upload

---
+++ http/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test http</title>
+</head>
+<body>
+    <?php
+       
+       $nome=$_GET["nome"];
+       $idade=$_GET["idade"];
+       echo "O meu nome é ", $nome," e a minha idade é  ", $idade;
+
+    ?>
+</body>
+</html>
